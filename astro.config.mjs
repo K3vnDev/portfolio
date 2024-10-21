59dd0eaac602a4a41a5371f17ef47767e4359044
K3vnDev <158790560+K3vnDev@users.noreply.github.com>
2024-10-20 20:36:06 -0500

    Update Astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// @ts-check
+import netlify from '@astrojs/netlify'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind'
-
-import react from '@astrojs/react'
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [tailwind(), react()]
 })
